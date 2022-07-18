f23e31eaebb418c6c2608e387a168a9231a9554c
SMKim94 <floor3th@gmail.com>
2022-07-19 00:14:42 +0900

    First commit

---
+++ csv-to-xlsx.go
@@ -0,0 +1,55 @@
+package csvtoxsx
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type CSVToXLSXError struct {
+	ErrorCode int
+	message   string
+}
+
+func (e *CSVToXLSXError) Error() string {
+	prefix := fmt.Sprintf("[CSVToXLSX Error: %d]", e.ErrorCode)
+	switch e.ErrorCode {
+	case -1:
+		e.message = "Failed to load CSV file."
+	case -2:
+		e.message = "Failed to read CSV file."
+	default:
+		e.message = "Unknown Error"
+	}
+	return fmt.Sprintf("%s %s", prefix, e.message)
+}
+
+func NewCSVToXLSXError(ErrorCode int) *CSVToXLSXError {
+	return &CSVToXLSXError{ErrorCode: ErrorCode, message: ""}
+}
+
+func LoadCSV(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, NewCSVToXLSXError(-1)
+	}
+
+	reader := csv.NewReader(bufio.NewReader(file))
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return nil, NewCSVToXLSXError(-2)
+	}
+
+	return rows, nil
+}
+
+func ConvertCSVToXLSX(csvPath string, xlsxPath string) {
+	rows, _ := LoadCSV(csvPath)
+	for i, row := range rows {
+		for k := range row {
+			fmt.Printf("%s", rows[i][k])
+		}
+		fmt.Println("")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SMKim94/csv-to-xlsx
+
+go 1.18
